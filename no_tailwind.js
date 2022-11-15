7e8170afe1e9c1aeb2ee221e4f06fdf520b8453f
Dustin Michels <dustin7538@gmail.com>
2022-11-14 17:05:06 -0700

    update files

--- no_tailwind.js
+++ no_tailwind.js
@@ -1,13 +1,11 @@
 /**
  * Default settings for formatting Journey projects.
+ * That don't use tailwind.
  */
 
 module.exports = {
   ...require('./core.js'),
-  plugins: [
-    require('@trivago/prettier-plugin-sort-imports'),
-    require('prettier-plugin-tailwindcss'),
-  ],
+  plugins: [require('@trivago/prettier-plugin-sort-imports')],
   importOrder: ['^env$', '^([a-zA-Z0-9]+)$', '^@[a-zA-Z]+/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
